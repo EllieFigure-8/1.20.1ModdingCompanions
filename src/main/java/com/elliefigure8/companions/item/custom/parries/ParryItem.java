81a587d62aac9e7b066eaf456b543444c5d1cde5
EllieFigure-8 <bruno@bertu.com.ar>
2024-10-06 02:44:42 -0300

    Now Ticks are consistent!

--- src/main/java/com/elliefigure8/companions/item/custom/parries/ParryItem.java
+++ src/main/java/com/elliefigure8/companions/item/custom/parries/ParryItem.java
@@ -50,7 +50,7 @@ public class ParryItem extends Item
             else
             {
                 hasPressedParry = false;
-            }
+            } //
         }
 
         if (!hasParry) {
