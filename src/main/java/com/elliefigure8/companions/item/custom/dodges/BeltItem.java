3097fc0bcdacff76b6724be8e3ab02f62ea0edae
EllieFigure-8 <bruno@bertu.com.ar>
2024-10-14 23:02:34 -0300

    Idk. I fixed the BeltItem which was already fixed.

--- src/main/java/com/elliefigure8/companions/item/custom/dodges/BeltItem.java
+++ src/main/java/com/elliefigure8/companions/item/custom/dodges/BeltItem.java
@@ -1,9 +1,13 @@
 package com.elliefigure8.companions.item.custom.dodges;
 
+import com.elliefigure8.companions.events.BeltDodgeEvent;
 import com.elliefigure8.companions.item.ModItems;
 import com.elliefigure8.companions.registry.KeyBindRegistry;
 import com.elliefigure8.companions.sound.ModSounds;
+import com.elliefigure8.companions.util.InventoryUtil;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
@@ -13,6 +17,7 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class BeltItem extends Item {
@@ -32,6 +37,7 @@ public class BeltItem extends Item {
     public static boolean RedBeltParryUsed = false;
     public static int RedBeltParrySharedCooldown = 300;
 
+
     @Override
     public void onInventoryTick(ItemStack stack, Level level, Player player, int slotIndex, int selectedIndex)
     {
@@ -39,7 +45,7 @@ public class BeltItem extends Item {
         {
             //boolean beltCanParry = player.getInventory().contains(new ItemStack(ModItems.BLACK_BELT.get()));
 
-            if (KeyBindRegistry.DashAbilityKey.consumeClick())
+            /*if (KeyBindRegistry.DashAbilityKey.consumeClick())
             {
                 System.out.println("canDodge: " + canDodge);
                 System.out.println("dodgeCooldown: " + dodgeCooldown);
@@ -51,8 +57,7 @@ public class BeltItem extends Item {
                 System.out.println("RedBeltParryUsed: " + RedBeltParryUsed);
                 System.out.println("RedBeltParrySharedCooldown: " + RedBeltParrySharedCooldown);
                 //System.out.println("beltCanParry: " + beltCanParry);
-            }
-
+            }*/
 
             if (KeyBindRegistry.ParryAbilityKey.consumeClick() && hasParry && !hasPressedParry)
             {
@@ -99,6 +104,7 @@ public class BeltItem extends Item {
             {
                 if (dodgeCooldown > 0) {
                     dodgeCooldown--;
+                    addDataToBelt(player, dodgeCooldown);
                 }
                 else
                 {
@@ -119,12 +125,23 @@ public class BeltItem extends Item {
             }
         }
     }
+
     public static int calculateCooldown(int roundedDamage)
     {
         int ticksCooldown = roundedDamage * 270;
         if (ticksCooldown > 1800) {ticksCooldown = 1800;}
         if (ticksCooldown < 300) {ticksCooldown = 300;}
         return ticksCooldown;
+    }
+
+    private void addDataToBelt(Player player, int dodgeCooldown)
+    {
+        ItemStack beltEquipped = player.getInventory().getItem(InventoryUtil.getFirstInventoryIndex(player, ModItems.BLACK_BELT.get()));
+
+        CompoundTag data = new CompoundTag();
+        data.putString("tooltip.companionsmod.add_data_to_belt.tooltip", "Dodge Cooldown: " + dodgeCooldown / 20 + " Seconds.");
+
+        beltEquipped.setTag(data);
     }
 
     @Override
@@ -157,6 +174,14 @@ public class BeltItem extends Item {
                 pTooltipComponents.add(Component.translatable("tooltip.companionsmod.black_belt.tooltip.shift"));
             }
         }
+        else if (Screen.hasControlDown())
+        {
+            if (pStack.getItem() == ModItems.BLACK_BELT.get() && pStack.hasTag())
+            {
+                String currentDodgeCooldown = pStack.getTag().getString("tooltip.companionsmod.add_data_to_belt.tooltip");
+                pTooltipComponents.add(Component.literal(currentDodgeCooldown));
+            }
+        }
         else
         {
             if (pStack.getItem() == ModItems.WHITE_BELT.get())
@@ -182,6 +207,11 @@ public class BeltItem extends Item {
             else if (pStack.getItem() == ModItems.BLACK_BELT.get())
             {
                 pTooltipComponents.add(Component.translatable("tooltip.companionsmod.black_belt.tooltip"));
+
+                if (pStack.hasTag() && pStack.getTag().contains("tooltip.companionsmod.add_data_to_belt.tooltip") && dodgeCooldown > 0) {
+                    String currentDodgeCooldown = pStack.getTag().getString("tooltip.companionsmod.add_data_to_belt.tooltip");
+                    pTooltipComponents.add(Component.literal(currentDodgeCooldown).withStyle(ChatFormatting.RED));
+                }
             }
         }
         super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
